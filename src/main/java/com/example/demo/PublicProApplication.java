4a9f769dd592033935484172e11e508e2a23b21c
harithasan <harirajan8940@gmail.com>
2023-05-22 14:00:55 +0530

    public commit

---
+++ src/main/java/com/example/demo/PublicProApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicProApplication.class, args);
+	}
+
+}
